c0548a65b4d93e87a6d388e2e26cb42f2b18b2f0
staneee <yi.hang@live.com>
2022-03-08 17:54:06 +0800

    启动注释

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+// 启动组件
+import App from './App.vue';
 
-import App from "./App.vue";
+// 路由定义
 import router from "./router";
 
+// 全局样式
 import '@/styles/index.scss';
 
+// 入口组件
 const app = createApp(App);
 
+// 挂载插件
 app.use(createPinia());
 app.use(router);
 
+// 启动
 app.mount("#app");
