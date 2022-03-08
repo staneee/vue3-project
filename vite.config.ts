092f20f0d6e70a21c6cc5f03908079627c8688cb
staneee <yi.hang@live.com>
2022-03-08 16:03:42 +0800

    element-plus组件按需编译插件和sass配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,35 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+// ElementPlus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  css:{
+    preprocessorOptions:{
+      scss: {
+        additionalData: `@use "~/styles/element/index.scss" as *;`,
+      },
+    }
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+    // ElementPlus
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  
 });
